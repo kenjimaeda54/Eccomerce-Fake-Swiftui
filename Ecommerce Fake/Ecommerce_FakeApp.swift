7e6a3eba7dc1bcda23c8d86a85b04a4535f8935f
kenjimaeda54 <kenjimaedafamily@gmail.com>
2023-01-28 20:35:09 -0300

    Initial Commit

---
+++ Ecommerce Fake/Ecommerce_FakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecommerce_FakeApp.swift
+//  Ecommerce Fake
+//
+//  Created by kenjimaeda on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Ecommerce_FakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
